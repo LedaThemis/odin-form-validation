90bee3d54583ae2b38187fca0ebe426e2245652a
Leda <ledathemis@protonmail.com>
2022-04-06 15:03:26 +0300

    add email input listener on blur

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ const DOMHandlers = (() => {
     e.preventDefault();
   };
 
+  const handleEmailBlur = (e) => {
+    e.preventDefault();
+    const emailInput = e.target;
+    if (emailInput.validity.typeMismatch) {
+      console.log(emailInput.validity.typeMismatch);
+      emailInput.reportValidity();
+    }
+  };
+
   return {
     handleFormSubmit,
+    handleEmailBlur,
   };
 })();
 
+const countries = fetch('https://restcountries.com/v3.1/all').then((res) => console.log(res));
+
 const submitButton = document.querySelector('#submit-button');
 submitButton.addEventListener('click', DOMHandlers.handleFormSubmit);
+
+const emailInput = document.querySelector('#email');
+emailInput.addEventListener('blur', DOMHandlers.handleEmailBlur);
